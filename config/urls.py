fc5332b352aaa78300dab2bf0fc5f1516d69df5a
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-09-28 00:14:26 +0300

    Implemented basic logic and user model

---
+++ config/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/station/", include("restaurant.urls", namespace="restaurant")),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
