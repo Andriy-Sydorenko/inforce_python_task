3bf5343371faf8da8425a564f9d1a3797aed4192
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-09-28 06:21:11 +0300

    updated requirements.txt and README.md

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7d-n!xj4&r$a(udl4i%r9duz4rv)m13c*sevvq5$xc89mhz1+e"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -86,8 +90,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
 }
 
@@ -147,10 +154,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    # "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.HeaderVersioning",
-    # "DEFAULT_VERSION": "1.0",  # Default version in case the client doesn't send any
-    # "ALLOWED_VERSIONS": ["1.0", "2.0"],  # The versions API currently supports
-    # "VERSION_PARAM": "version",  # This is the default value, used in the Accept header
 }
 
 SPECTACULAR_SETTINGS = {
@@ -167,6 +170,6 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),  # default - 5 minutes
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),  # default - 5 minutes
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),    # default - 1 day
 }
